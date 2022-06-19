208ca9adefcab85dcc1ade4824cd8c0f081099ef
latifaahu <elhaashabiyya8081@student.uns.ac.id>
2022-06-19 10:38:07 +0700

    update title

--- resources/views/pasar/edit-pasar.blade.php
+++ resources/views/pasar/edit-pasar.blade.php
@@ -4,7 +4,7 @@
 
 <main class="h-full pb-16 overflow-y-auto">
     <div class="container grid px-6 mx-auto">
-        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Tambah Pasar</h2>
+        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Update Pasar</h2>
 
         <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
             <form method="POST" action="/pasar/{{ $pasar->id }}">
